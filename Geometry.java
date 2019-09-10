69dce30094ae167694f9eba08eb1d8cfc4bc078a
badninr <badninr@wlhs.wlwv.k12.or.us>
2019-09-10 09:35:22 -0700

    Added geometry class

--- Geometry.java
+++ Geometry.java
@@ -2,7 +2,7 @@ public class Geometry{
     public static void main (String[] args){
         int sides=7;
         int radius=5;
-        final double PI= 3.1415;
+        final double PI= 3.14159265359;
         System.out.println("A heptagon has "+sides+" sides");
         sides=10;
         System.out.println("A decagon has "+sides+" sides");
@@ -10,5 +10,13 @@ public class Geometry{
         System.out.println("A dodecagon has "+sides+" sides");
         System.out.println("The circumference of a circle with a radius of "
         +radius+" is "+ (2*PI*radius));
+        
+        int a=40;
+        double b=6.0;
+        double y= 6.0;
+        double z;
+        z= a/b;
+       
+        System.out.println("z="+z);
     }        
 }
